66cfa7bfa7ff13d96ae773ab62674b0bd71032da
xianghao <wang.xianghao@outlook.com>
2025-10-10 23:40:10 +0800

    fix format

--- src/v02_input_tiling.cu
+++ src/v02_input_tiling.cu
@@ -12,8 +12,10 @@ __global__ void convolution_v02(size_t m, size_t n, size_t r, T const* A,
     const size_t OUTPUT_BLOCK_Y{INPUT_BLOCK_Y - 2U * r};
     const ssize_t tile_row{threadIdx.y - r_ll};
     const ssize_t tile_col{threadIdx.x - r_ll};
-    const ssize_t row{static_cast<ssize_t>(blockIdx.y * OUTPUT_BLOCK_Y) + tile_row};
-    const ssize_t col{static_cast<ssize_t>(blockIdx.x * OUTPUT_BLOCK_X) + tile_col};
+    const ssize_t row{static_cast<ssize_t>(blockIdx.y * OUTPUT_BLOCK_Y) +
+                      tile_row};
+    const ssize_t col{static_cast<ssize_t>(blockIdx.x * OUTPUT_BLOCK_X) +
+                      tile_col};
 
     // Access constant memory
     T const* W_const = reinterpret_cast<T const*>(const_mem);
