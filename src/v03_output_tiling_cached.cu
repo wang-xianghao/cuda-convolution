8e852ffc1e3209249c43c81afd9294be4168f0b3
xianghao <wang.xianghao@outlook.com>
2025-10-11 18:55:19 +0800

    finish v03 kernel using output tiling

--- src/v03_output_tiling_cached.cu
+++ src/v03_output_tiling_cached.cu
@@ -1,40 +1,71 @@
 #include "cuda_convolution.hpp"
 #include "cuda_convolution_utils.hpp"
 
-template <typename T>
+template <typename T, size_t OUTPUT_BLOCK_X, size_t OUTPUT_BLOCK_Y>
 __global__ void convolution_v03(size_t m, size_t n, size_t r, T const* A,
                                 size_t lda, T* B, size_t ldb, T const* W,
                                 size_t ldw)
 {
-    const size_t B_col_idx{blockIdx.x * blockDim.x + threadIdx.x};
-    const size_t B_row_idx{blockIdx.y * blockDim.y + threadIdx.y};
+    const ssize_t r_signed{static_cast<ssize_t>(r)};
+    const size_t k{2U * r + 1U};
+    const size_t row{blockIdx.y * OUTPUT_BLOCK_Y + threadIdx.y};
+    const size_t col{blockIdx.x * OUTPUT_BLOCK_X + threadIdx.x};
+    const size_t thread_row{threadIdx.y};
+    const size_t thread_col{threadIdx.x};
 
     // Access constant memory
     T const* W_const = reinterpret_cast<T const*>(const_mem);
 
-    if (B_row_idx >= m || B_col_idx >= n)
+    // Copy tile to shared memory
+    __shared__ T A_tile[OUTPUT_BLOCK_Y][OUTPUT_BLOCK_X];
+    if (row < m && col < n)
     {
-        return;
+        A_tile[thread_row][thread_col] = A[row * lda + col];
     }
-
-    T sum{static_cast<T>(0)};
-
-    for (size_t w_row_idx{0}; w_row_idx < 2U * r + 1U; w_row_idx++)
+    else
     {
-        for (size_t w_col_idx{0}; w_col_idx < 2U * r + 1U; w_col_idx++)
+        A_tile[thread_row][thread_col] = static_cast<T>(0);
+    }
+    __syncthreads();
+
+    // Compute cell
+    if (row < m && col < n)
+    {
+        T sum{static_cast<T>(0)};
+        for (size_t w_row{0U}; w_row < k; ++w_row)
         {
-            ssize_t A_row_idx{static_cast<ssize_t>(B_row_idx - r + w_row_idx)};
-            ssize_t A_col_idx{static_cast<ssize_t>(B_col_idx - r + w_col_idx)};
-            if (A_row_idx >= 0 && A_row_idx < m && A_col_idx >= 0 &&
-                A_col_idx < n)
+            for (size_t w_col{0U}; w_col < k; ++w_col)
             {
-                sum += A[A_row_idx * lda + A_col_idx] *
-                       W_const[w_row_idx * ldw + w_col_idx];
+                const ssize_t input_row{
+                    static_cast<ssize_t>(row - r_signed + w_row)};
+                const ssize_t input_col{
+                    static_cast<ssize_t>(col - r_signed + w_col)};
+                const ssize_t input_thread_row{
+                    static_cast<ssize_t>(thread_row - r_signed + w_row)};
+                const ssize_t input_thread_col{
+                    static_cast<ssize_t>(thread_col - r_signed + w_col)};
+                if (input_thread_row >= 0 &&
+                    input_thread_row < OUTPUT_BLOCK_Y &&
+                    input_thread_col >= 0 && input_thread_col < OUTPUT_BLOCK_X)
+                {
+                    // Use shared memory
+                    sum += A_tile[input_thread_row][input_thread_col] *
+                           W_const[w_row * ldw + w_col];
+                }
+                else
+                {
+                    // Use L2 cache
+                    if (input_row >= 0 && input_row < m && input_col >= 0 &&
+                        input_col < n)
+                    {
+                        sum += A[input_row * lda + input_col] *
+                               W_const[w_row * ldw + w_col];
+                    }
+                }
             }
         }
+        B[row * ldb + col] = sum;
     }
-
-    B[B_row_idx * ldb + B_col_idx] = sum;
 }
 
 template <typename T>
@@ -42,11 +73,13 @@ void launch_kernel_convolution_v03(size_t m, size_t n, size_t r, T const* A,
                                    size_t lda, T* B, size_t ldb, T const* W,
                                    size_t ldw, cudaStream_t stream)
 {
-    const dim3 block_dim{32U, 32U, 1U};
+    const size_t OUTPUT_BLOCK_X{32U};
+    const size_t OUTPUT_BLOCK_Y{32U};
+    const dim3 block_dim{OUTPUT_BLOCK_X, OUTPUT_BLOCK_Y, 1U};
     const dim3 grid_dim{
         (static_cast<unsigned int>(m) + block_dim.x - 1U) / block_dim.x,
         (static_cast<unsigned int>(n) + block_dim.y - 1U) / block_dim.y, 1U};
-    convolution_v03<T>
+    convolution_v03<T, OUTPUT_BLOCK_X, OUTPUT_BLOCK_Y>
         <<<grid_dim, block_dim, 0U, stream>>>(m, n, r, A, lda, B, ldb, W, ldw);
     CHECK_LAST_CUDA_ERROR();
 }
